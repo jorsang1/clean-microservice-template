1638e2b623bcd927f0535f01ffbbfb0ca2ff2657
Robert Synoradzki <robsharp.robert.synoradzki@gmail.com>
2022-04-24 11:35:52 +0200

    Style: Use file-scoped namespaces everywhere

--- src/Api.Contracts/Requests/AddProductRequest.cs
+++ src/Api.Contracts/Requests/AddProductRequest.cs
@@ -1,21 +1,20 @@
 ﻿using System;
 
-namespace Api.Contracts.Requests
+﻿namespace Api.Contracts.Requests;
+
+public class AddProductRequest
 {
-    public class AddProductRequest
+    public Guid Id { get; set; }
+    public string Sku { get; set; }
+    public string Title { get; set; }
+    public string? Description { get; set; }
+
+    public AddProductRequest(Guid id, string sku, string title, string? description)
     {
-        public Guid Id { get; set; }
-        public string Sku { get; set; }
-        public string Title { get; set; }
-        public string? Description { get; set; }
-
-        public AddProductRequest(Guid id, string sku, string title, string? description)
-        {
-            Id = id;
-            Sku = sku;
-            Title = title;
-            Description = description;
-        }
-
+        Id = id;
+        Sku = sku;
+        Title = title;
+        Description = description;
     }
+
 }
--- src/Api.Contracts/Responses/ProductResponse.cs
+++ src/Api.Contracts/Responses/ProductResponse.cs
@@ -1,22 +1,21 @@
 ﻿using System;
 
-namespace Api.Contracts.Responses
-{
-    public class ProductResponse
-    {
-        public Guid Id { get; set; }
-        public string Sku { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public decimal Price { get; set; }
+﻿namespace Api.Contracts.Responses;
 
-        public ProductResponse(Guid id, string sku, string title, string description, decimal price)
-        {
-            Id = id;
-            Sku = sku;
-            Title = title;
-            Description = description;
-            Price = price;
-        }
+public class ProductResponse
+{
+    public Guid Id { get; set; }
+    public string Sku { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+
+    public ProductResponse(Guid id, string sku, string title, string description, decimal price)
+    {
+        Id = id;
+        Sku = sku;
+        Title = title;
+        Description = description;
+        Price = price;
     }
 }
